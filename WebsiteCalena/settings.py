95a3cf4adc2f23f1fcab865930053267ed5a3046
Sofia Donlucas Bañuelos <sof.db@outlook.com>
2023-07-21 22:16:13 -0600

    files addes

--- WebsiteCalena/settings.py
+++ WebsiteCalena/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/CalenaApp'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Configure the directory where static files will be collected for deployment
