10e684c5e0844b3c9175647ac389c15de6690e78
Sofia Donlucas Bañuelos <sof.db@outlook.com>
2023-07-21 18:31:46 -0600

    commit

--- WebsiteCalena/settings.py
+++ WebsiteCalena/settings.py
@@ -119,8 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/CalenaApp'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
+# Configure the directory where static files will be collected for deployment
+STATIC_ROOT = BASE_DIR / "staticfiles_build"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
